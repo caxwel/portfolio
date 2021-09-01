c98af9643f6e1e054659ef0e2cdbce23dba9cf4c
Max Turer <turerhal@gmail.com>
2021-09-02 03:10:03 +0900

    script edit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/", router);
 app.use(express.static(path.join(__dirname, "../react-ui/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "../react-ui/build/index.html"));
 });
 
